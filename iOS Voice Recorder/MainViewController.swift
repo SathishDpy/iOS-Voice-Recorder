b2f081047b919ec7637abc10b5ce4d822589e8b3
Sathish Kumar <sathish@jupitersupplystation.com>
2020-05-12 06:45:44 +0530

    Add audio session code to record the audio.

--- iOS Voice Recorder/MainViewController.swift
+++ iOS Voice Recorder/MainViewController.swift
@@ -7,23 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, AVAudioRecorderDelegate {
   
   @IBOutlet weak var startOrStopButton: UIButton!
-  var isRecording = false
+  var recordingSession: AVAudioSession!
+  var audioRecorder: AVAudioRecorder!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    startOrStopButton.isHidden = true
+    recordingSession = AVAudioSession.sharedInstance()
+
+    do {
+      try recordingSession.setCategory(.playAndRecord, mode: .default, options: [])
+      try recordingSession.setActive(true, options: [])
+      recordingSession.requestRecordPermission { [unowned self] allowed in
+        DispatchQueue.main.async {
+          if allowed {
+            self.startOrStopButton.isHidden = false
+          } else {
+            print("Failed to get the permisssion")
+          }
+        }
+      }
+    } catch {
+      print("Failed to set audio category")
+    }
   }
   
   @IBAction func startOrStopButtonClicked(_ sender: UIButton) {
-    isRecording = !isRecording
-    if isRecording {
-      sender.setTitle("Recording/Stop", for: .normal)
+    if audioRecorder == nil {
+      startRecording()
     } else {
-      sender.setTitle("Start", for: .normal)
+      finishRecording(true)
     }
   }
+
+  func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    return paths[0]
+  }
+
+  func startRecording() {
+    let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+    let settings = [
+      AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+      AVSampleRateKey: 12000,
+      AVNumberOfChannelsKey: 1,
+      AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+    ]
+
+    do {
+      audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+      audioRecorder.delegate = self
+      audioRecorder.record()
+    } catch {
+      finishRecording(false)
+    }
+
+  }
+
+  func finishRecording(_ success: Bool) {
+    audioRecorder.stop()
+    audioRecorder = nil
+    if success {
+      startOrStopButton.setTitle("Record", for: .normal)
+    } else {
+      startOrStopButton.setTitle("Stop Record", for: .normal)
+    }
+  }
+
+
+  // MARK: delegate methods
+
+  func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+    if !flag {
+      finishRecording(false)
+    }
+  }
+
 }
